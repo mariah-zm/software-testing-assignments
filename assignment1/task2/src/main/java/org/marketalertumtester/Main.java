289e2096763bc530e5936821f48cb0f65eac375f
mariahz <mariahz@ccbill.com>
2022-11-13 11:11:15 +0100

    Created two different directories for tasks

---
+++ assignment1/task2/src/main/java/org/marketalertumtester/Main.java
@@ -0,0 +1,7 @@
+package org.marketalertumtester;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ assignment1/task2/src/test/java/org/marketalertumtester/LoginSteps.java
@@ -0,0 +1,9 @@
+package org.marketalertumtester;
+
+import io.cucumber.java.en.Given;
+
+public class LoginSteps {
+    @Given("I am a user of MarketAlertUm")
+    public void iAmAUserOfMarketAlertUm() {
+    }
+}
